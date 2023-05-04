6eef6ceac5ec43a90432b362bab86e81fd8b0f51
Arturo Rivera <arturo.riverar97@gmail.com>
2023-05-03 19:55:10 -0600

    Set up Search and WeatherCard components/styles

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import Search from './src/components/Search';
+import WeatherCard from './src/components/WeatherCard';
+import backgroundBlue from './src/Assets/Background/Bg2.jpg'
+import { useState } from 'react';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.mainView}>
+      <Image source={backgroundBlue} style={styles.bgImage}></Image>
+      <View style={styles.appContainer}>
+        <Search />
+        <WeatherCard temperature={40} place = {'Puebla'} date = {`${new Date()}`.substring(0,10)} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainView: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#212120',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  appContainer:{
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '100%'
+  },
+  bgImage: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top:0,
+    left:0,
+    zIndex: -10
+  }
+})
