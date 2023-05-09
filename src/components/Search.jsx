d805d27b2e25f524ca191369cca72735969bbfd8
Arturo Rivera <arturo.riverar97@gmail.com>
2023-05-08 19:36:17 -0600

    Modify View border and set placeholder color to white

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import { View, TextInput, StyleSheet } from 'react-native'
 import Constants from 'expo-constants'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { useState } from 'react'
-import { TouchableOpacity } from 'react-native-web'
+import { TouchableOpacity } from 'react-native'
 
 const Search = ({setCity}) => {
 
@@ -14,9 +14,10 @@ const Search = ({setCity}) => {
          value = {localCity}
          onChangeText = {(city) => setLocalCity(city)}
          placeholder = {'Search city'}
+         placeholderTextColor="white" 
          style = {styles.input}></TextInput>
-        <TouchableOpacity onPress={() => setCity(localCity)}>
-          <Icon name='search' size={28} color='white' />
+         <TouchableOpacity onPress={() => setCity(localCity)}>
+          <Icon name='search' size={28} color='white'/>
         </TouchableOpacity>
     </View>
   )
@@ -26,13 +27,12 @@ const styles = StyleSheet.create({
     search: {
         backgroundColor: 'transparent',
         width: '100%',
-        borderRadius:10,
         marginTop: Constants.statusBarHeight + 15,
         paddingVertical: 10,
         paddingHorizontal: 8,
         borderColor: 'white',
-        borderWidth: 2,
-        flexDirection: 'row'
+        borderBottomWidth: 2,
+        flexDirection: 'row',
     },
     input: {
         flex: 1,
