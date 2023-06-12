dd430d52ebd5b834302aa82d9343ed147b80abd4
Arturo Rivera <arturo.riverar97@gmail.com>
2023-06-11 21:10:36 -0600

    Serve weather data using the React API Context

--- React/src/components/WeatherData.tsx
+++ React/src/components/WeatherData.tsx
@@ -1,34 +1,39 @@
 import { weatherIcons } from '../assets/weatherIcons'
 import '../styles/WeatherData.css'
 import test from '../assets/Puebla.jpg'
+import { weatherContext } from '../App'
+import { useContext } from 'react'
 
 const WeatherData = () => {
+
+  const {weatherData} = useContext(weatherContext)
+
   return (
     <div className="weather-data-container">
-        <img src={weatherIcons['Clear']} alt="sun" className='icon' />
+        <img src={weatherData.hasData ? weatherIcons[weatherData.current.icon] : 'Clear' } alt="sun" className='icon' />
         <div className="temperature-container">
-          <span className='temperature'>20</span>
+          <span className='temperature'>{weatherData.hasData ? weatherData.current.temperature : 20}</span>
           <span className='unit'>°C</span>
         </div>
         <div className="day-hour-container">
-            <span>Monday,</span>
+            <span>{weatherData.hasData && weatherData.current.day},</span>
             <span className='hour'>16:00</span>
         </div>
         <div className='extra-data-container'>
           <div className='description-icon'>
-            <img src={weatherIcons['Tornado']} alt="icon" className='icon-extra-data' />
-            <span>Mostly Coudy</span>
+            <img src={weatherData.hasData ? weatherIcons[weatherData.current.icon] : 'Clear'} alt="icon" className='icon-extra-data' />
+            <span>{weatherData.hasData && weatherData.current.description}</span>
           </div>
           <div className='description-icon'>
             <img src={weatherIcons['Humidity']} alt="icon" className='icon-extra-data' />
             <span>Humidity</span>
             <span>-</span>
-            <span>30%</span>
+            <span>{weatherData.hasData && weatherData.current.humidity}%</span>
           </div>
           <div className='city-img-container'>
             <img src={test} className='place-img'></img>
             <div className='img-opacity'></div>
-            <span className='city'>Puebla</span>
+            <span className='city'>{weatherData.hasData && weatherData.current.city}</span>
           </div>
         </div>
     </div>
