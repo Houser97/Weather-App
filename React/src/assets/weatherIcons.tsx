880716ccd34c9a3c85ae17cb1dfca8c301e4692e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-06-14 21:22:40 -0600

    Set up Icons to be used in WeatherData component

---
+++ React/src/assets/weatherIcons.tsx
@@ -0,0 +1,35 @@
+import Clear from './icons/fill/clear-day.svg'
+import ClearNight from './icons/fill/clear-night.svg'
+import Drizzle from './icons/fill/drizzle.svg'
+import Haze from './icons/fill/haze.svg'
+import HazeNight from './icons/fill/haze-night.svg'
+import Mist from './icons/fill/mist.svg'
+import Rain from './icons/fill/rain.svg'
+import Smoke from './icons/fill/smoke.svg'
+import Snow from './icons/fill/snow.svg'
+import Thunderstorm from './icons/fill/thunderstorms.svg'
+import Tornado from './icons/fill/tornado.svg'
+import Dust from './icons/fill/dust.svg'
+import Fog from './icons/fill/fog.svg'
+import Celsius from './icons/fill/celsius.svg'
+import Humidity from './icons/humidity.svg'
+import Clouds from './icons/overcast.svg'
+
+export const weatherIcons = {
+    Thunderstorm,
+    ClearNight,
+    HazeNight,
+    Drizzle,
+    Rain,
+    Snow,
+    Tornado,
+    Clear,
+    Clouds,
+    Mist,
+    Haze,
+    Dust,
+    Fog,
+    Smoke,
+    Celsius,
+    Humidity
+}
