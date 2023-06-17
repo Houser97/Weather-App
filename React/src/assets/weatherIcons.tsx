6f765a722dba6cee0df0d33b4d49fb7d992708b8
Arturo Rivera <arturo.riverar97@gmail.com>
2023-06-16 22:28:05 -0600

    Import night and day icons and remove unused ones

--- React/src/assets/weatherIcons.tsx
+++ React/src/assets/weatherIcons.tsx
@@ -1,35 +1,54 @@
-import Clear from './icons/fill/clear-day.svg'
-import ClearNight from './icons/fill/clear-night.svg'
-import Drizzle from './icons/fill/drizzle.svg'
-import Haze from './icons/fill/haze.svg'
-import HazeNight from './icons/fill/haze-night.svg'
-import Mist from './icons/fill/mist.svg'
-import Rain from './icons/fill/rain.svg'
-import Smoke from './icons/fill/smoke.svg'
-import Snow from './icons/fill/snow.svg'
-import Thunderstorm from './icons/fill/thunderstorms.svg'
-import Tornado from './icons/fill/tornado.svg'
-import Dust from './icons/fill/dust.svg'
-import Fog from './icons/fill/fog.svg'
-import Celsius from './icons/fill/celsius.svg'
-import Humidity from './icons/humidity.svg'
-import Clouds from './icons/overcast.svg'
+import Clear from './icons/weather/clear-day.svg'
+import ClearNight from './icons/weather/clear-night.svg'
+import Drizzle from './icons/weather/dayDrizzle.svg'
+import DrizzleNight from './icons/weather/nightDrizzle.svg'
+import Haze from './icons/weather/hazeDay.svg'
+import HazeNight from './icons/weather/hazeNight.svg'
+import Mist from './icons/weather/mist.svg'
+import Rain from './icons/weather/dayRain.svg'
+import RainNight from './icons/weather/nightRain.svg'
+import Smoke from './icons/weather/smokeDay.svg'
+import SmokeNight from './icons/weather/smokeNight.svg'
+import Snow from './icons/weather/snowDay.svg'
+import SnowNight from './icons/weather/snowNight.svg'
+import Thunderstorm from './icons/weather/thunderstormsDay.svg'
+import ThunderstormNight from './icons/weather/thunderstormsNight.svg'
+import Tornado from './icons/weather/tornado.svg'
+import Dust from './icons/weather/dustDay.svg'
+import DustNight from './icons/weather/dustNight.svg'
+import Fog from './icons/weather/fogDay.svg'
+import FogNight from './icons/weather/fogNight.svg'
+import Humidity from './icons/general/humidity.svg'
+import Clouds from './icons/weather/cloudyDay.svg'
+import CloudsNight from './icons/weather/cloudyNight.svg'
 
 export const weatherIcons = {
+    //Day
     Thunderstorm,
-    ClearNight,
-    HazeNight,
     Drizzle,
     Rain,
     Snow,
-    Tornado,
     Clear,
     Clouds,
-    Mist,
     Haze,
     Dust,
     Fog,
     Smoke,
-    Celsius,
+
+    //Night
+    ThunderstormNight,
+    DrizzleNight,
+    ClearNight,
+    RainNight,
+    SmokeNight,
+    SnowNight,
+    DustNight,
+    FogNight,
+    CloudsNight,
+    HazeNight,
+
+    //General
+    Tornado,
+    Mist,
     Humidity
 }
