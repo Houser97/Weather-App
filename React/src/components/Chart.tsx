d13a09764d44975dfe0e247dc26a27d39185d7ac
Arturo Rivera <arturo.riverar97@gmail.com>
2023-06-19 19:52:38 -0600

    Retrieve forecast data to display it in the Chart

--- React/src/components/Chart.tsx
+++ React/src/components/Chart.tsx
@@ -6,23 +6,61 @@ import {
     CategoryScale,
     LinearScale,
     PointElement,
-    Legend,
+    Filler
 } from 'chart.js'
+import { useSelector } from 'react-redux'
+import { weatherDataSelector } from '../redux/slices/weather'
+import { useEffect, useState } from 'react'
+
+interface Accumulator {
+    labels: string[],
+    data: number[],
+    max: number,
+    min: number
+}
 
 ChartJS.register(
     LineElement,
     CategoryScale,
     LinearScale,
     PointElement,
-    Legend
+    //Filler,
 )
 
+const ChartMinMaxOffset = 0.5
+
 const Chart = () => {
+
+    const { current, forecastDaily } = useSelector(weatherDataSelector)
+    const [labels, setLabels] = useState(["Jan", "Feb", "Mar", "Apr", "May", "Jun"])
+    const [dataChart, setDataChart] = useState([4, 2, 5, 2, 8, 7])
+    const [maxValue, setMaxValue] = useState(40)
+    const [minValue, setMinValue] = useState(20)
+
+    useEffect(() => {
+      const data = forecastDaily.reduce((acc: Accumulator,forecast) => {
+        const temperature = forecast.temperature
+        const label = forecast.day
+        acc.labels = [...acc.labels, label]
+        acc.data = [...acc.data, temperature]
+        acc.max = Math.max(acc.max, temperature)
+        acc.min = Math.min(acc.min, temperature)
+        return acc
+      }, {labels: [], data: [], max: -Infinity, min: Infinity})
+
+      setLabels(data.labels)
+      setDataChart(data.data)
+      setMaxValue(data.max + ChartMinMaxOffset)
+      setMinValue(data.min - ChartMinMaxOffset)
+    
+    }, [current.city])
+    
+
     const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: labels,
     datasets: [{
       label: "First dataset",
-      data: [4, 2, 5, 2, 8, 7],
+      data: dataChart,
       fill: "start",
       backgroundColor: (context: any) => {
         const ctx = context.chart.ctx;
@@ -35,32 +73,29 @@ const Chart = () => {
       pointBorderColor: "#F28C38",
       pointBorderWidth: 1,
       tension: 0.5,
-      borderWidth: 4
+      borderWidth: 6
     }]
     }
 
     const options: any = {
         responsive: true,
+        maintainAspectRatio: false, //Permite especificar altura y ancho de gráfica en CSS.
         plugins: {
             legend: false
         },
         scales: {
-            xAxes: [{gridLines: { color: "green" }}],
-            yAxes: [{gridLines: { color: "#131c2b" }}],
             x: {
                 grid: {
                     display: false,
-                    color: 'gray',
-                    borderColor: 'green'
                 },
 
             },
             y: {
-                min: -2,
-                max: 10,
+                min: minValue,
+                max: maxValue,
                 ticks: {
                     stepSize: 2,
-                    callback: (value: number) => value + 'K'
+                    callback: (value: number) => value + '°C'
                 },
                 grid: {
                     display: false
