de19dfbec2afed9696342cd99be77e68df8c32b4
Arturo Rivera <arturo.riverar97@gmail.com>
2023-06-21 19:47:21 -0600

    Fill the Dashboard with data when it is opened for the first time

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,19 +1,33 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
 import Home from './components/Home'
+import { fetchWeatherDataRedux } from './redux/slices/weather'
+import { AppDispatch } from './redux/store'
 
 
 export const weatherContext = createContext({})
 
 function App() {
+
+  const dispatch = useDispatch<AppDispatch>()
   
   const [filter, setFilter] = useState('daily')
   const [filterDailyData, setFilterDailyData] = useState('set1')
   const [isLoading, setIsLoading] = useState(false) //Loading para componente Search
   const [isLoadingData, setIsLoadingData] = useState(true) //Loading para predicciones.
   const [cityError, setCityError] = useState(false)
+  const [fetchDataOnce, setFetchDataOnce] = useState(true)
   const [tempUnit, setTempUnit] = useState('metric') //El valor se pasa al objeto creado en la API en lugar de pasarlo
   //directo a los componente para garantizar que la aplicación se renderiza al mismo tiempo y no las unidades antes que el valor.
+
+  //useEffect que se usa para llenar el dashboard con datos de Puebla cuando se abre por primera vez.
+  useEffect(() => {
+    if(!fetchDataOnce) return undefined
+    setFetchDataOnce(false)
+    dispatch(fetchWeatherDataRedux('Puebla', 'metric'))
+  }, [fetchDataOnce])
+  
 
   const value = {}
 
