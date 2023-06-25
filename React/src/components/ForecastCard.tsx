4316fc1fedf748805e497fecd2ccb8e99302d32a
Arturo Rivera <arturo.riverar97@gmail.com>
2023-06-25 10:09:07 -0600

    Add units and render hourly or daily forecast data

--- React/src/components/ForecastCard.tsx
+++ React/src/components/ForecastCard.tsx
@@ -1,16 +1,15 @@
 import { weatherIcons } from '../assets/weatherIcons'
 import '../styles/ForecastCard.css'
-import { forecastType } from '../TypeScript/weatherTypes'
+import { forecastType, hourlyForecast, hourlyType } from '../TypeScript/weatherTypes'
 
 interface ForecastCardProps {
-    data: forecastType,
-    forecastType: string
+    data: hourlyForecast | forecastType,
 }
 
-const ForecastCard = ({data, forecastType}: ForecastCardProps) => {
+const ForecastCard = ({data}: ForecastCardProps) => {
 
     const icon = weatherIcons[data.weather]
-    const date = data.date
+    const dayHour = 'hour' in data ? data.hour : data.day
     const humidity = data.humidity
     const pressure = data.pressure
     const windSpeed = data.windSpeed
@@ -18,21 +17,21 @@ const ForecastCard = ({data, forecastType}: ForecastCardProps) => {
     
   return (
     <div className='forecast-card'>
-        <div className='forecast-day-hour'>{date}</div>
+        <div className='forecast-day-hour'>{dayHour}</div>
         <img src={icon} alt="weather-icon" className='forecast-weather-icon' />
         <div className='weather-data-forecast'>
             <div className='forecast-pressure-humidity'>
                 <span>
                     <img src={weatherIcons['Humidity']} alt="Humidity-icon" />
-                    {humidity}
+                    {humidity} %
                 </span>
                 <span>
                     <img src={weatherIcons['Barometer']} alt="Humidity-icon" />
-                    {pressure}
+                    {pressure} hPa
                 </span>
                 <span>
                     <img src={weatherIcons['WindSpeed']} alt="Humidity-icon" />
-                    {windSpeed}
+                    {windSpeed} km/h
                 </span>
             </div>
             <div className="temperature-container">
