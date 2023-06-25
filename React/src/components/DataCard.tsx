1d965e731b432b3fc171ee8cac21467066554e57
Arturo Rivera <arturo.riverar97@gmail.com>
2023-06-24 23:28:54 -0600

    Split strings containing two words
    This applies only for the case the component is displaying data about
    the 'feels like' section, since the valueTarget# contains two words
    in a single string.

--- React/src/components/DataCard.tsx
+++ React/src/components/DataCard.tsx
@@ -11,30 +11,37 @@ interface props {
   type: string
 }
 
+const getSecondWord = (word: string) => {
+  const wordArray = word.split(/(?=[A-Z])/)
+  return wordArray.length === 2 ? wordArray[1] : wordArray[0]
+}
+
 const DataCard = ({title, valueTarget1, valueTarget2, type}: props) => {
 
   const { current } = useSelector(weatherDataSelector)
   const value1 = current[valueTarget1]
   const value2 = current[valueTarget2]
-  const nameValue1 = capitalizeFirstLetter(valueTarget1)
-  const nameValue2 = capitalizeFirstLetter(valueTarget2)
+  const Icon1 = capitalizeFirstLetter(valueTarget1)
+  const Icon2 = capitalizeFirstLetter(valueTarget2)
+  const Description1 = getSecondWord(Icon1)
+  const Description2 = getSecondWord(Icon2)
   const unit = type === 'temperature' ? '°C' : ''
 
   return (
     <div className='datacard-container'>
         <h3>{title}</h3>
         <div className='icon-value'>
-            <img src={weatherIcons[nameValue1]} alt="icon" />
+            <img src={weatherIcons[Icon1]} alt="icon" />
             <div className='sunrise-hour'>
               <div className='sunrise-sunset-value'>{value1} {unit}</div>
-              {nameValue1}
+              {Description1}
             </div>
         </div>
         <div className='icon-value'>
-            <img src={weatherIcons[nameValue2]} alt="icon" />
+            <img src={weatherIcons[Icon2]} alt="icon" />
             <div className='sunrise-hour'>
               <div className='sunrise-sunset-value'>{value2} {unit}</div>
-              {nameValue2}
+              {Description2}
             </div>
         </div>
     </div>
