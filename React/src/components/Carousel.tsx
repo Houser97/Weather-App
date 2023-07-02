b209fb7db5c6936591b1ae9239e10e5824d2ed1d
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-02 14:31:48 -0600

    Change slides to show and scroll according to screen size

--- React/src/components/Carousel.tsx
+++ React/src/components/Carousel.tsx
@@ -6,18 +6,52 @@ import { useSelector } from "react-redux";
 import { filterSelector } from "../redux/slices/filter";
 import { weatherDataSelector } from "../redux/slices/weather";
 import { getforecastDailyData, getforecastHourlyData } from "../assets/FormatFunctions";
+import useWindowSize from "../assets/hooks/windowSize";
+import { useEffect, useState } from "react";
+
+const widthWindow = 2560
 
 const Carousel = () => {
 
-    const { forecastOption } = useSelector(filterSelector)
+    const windowSize = useWindowSize()
 
+    const { forecastOption } = useSelector(filterSelector)
     const { forecastDaily, forecastHourly, current } = useSelector(weatherDataSelector)
+
+    const CarouselInitialOptions = {
+      slidesToScroll: forecastOption === 'daily' ? 2 : 7,
+      slidesToShow: windowSize.width <= widthWindow ? 4 : 7
+    }
+
+    const [carouselOptions, setCarouselOptions] = useState(CarouselInitialOptions)
+
+    useEffect(() => {
+      let newOptions;
+      if(windowSize.width <= 1050){
+        newOptions = {
+          slidesToScroll: forecastOption === 'daily' ? 3 : 3,
+          slidesToShow: forecastOption === 'daily' ? 3 : 3
+        }
+      } else if(windowSize.width <= 1800){
+        newOptions = {
+          slidesToScroll: forecastOption === 'daily' ? 4 : 4,
+          slidesToShow: forecastOption === 'daily' ? 4 : 4
+        }
+      } else {
+        newOptions = {
+          slidesToScroll: forecastOption === 'daily' ? 2 : 5,
+          slidesToShow: forecastOption === 'daily' ? 5 : 5
+        }
+      }
+      setCarouselOptions(newOptions)
+    }, [windowSize.width, forecastOption])
+    
 
     let settings = {
         infinite: true,
         speed: 500,
-        slidesToShow: 7,
-        slidesToScroll: 2,
+        slidesToShow: carouselOptions.slidesToShow,
+        slidesToScroll: carouselOptions.slidesToScroll,
         autoplay: true,
         dots: true,
     }
