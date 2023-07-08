ff5a32a754275d54b2382170d187fbfbfa3a2ffd
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-08 16:57:40 -0600

    Set up Loading component

---
+++ React/src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import '../styles/Loading.css'
+
+const Loading = () => {
+  return (
+    <div className="loading__container">
+        <span className="loader"></span>
+    </div>
+  )
+}
+
+export default Loading
