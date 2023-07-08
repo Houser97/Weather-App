0b0493ac0a2d5d718844abda925faa4e5419d70e
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-08 16:58:15 -0600

    Import Loading component and fetch weather data when app is opened

--- React/src/components/Home.tsx
+++ React/src/components/Home.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchWeatherDataRedux, weatherDataSelector } from '../redux/slices/weather'
+import { AppDispatch } from '../redux/store'
 import '../styles/Home.css'
 import Dashboard from './Dashboard'
+import Loading from './Loading'
 import Sidebar from './Sidebar'
 
 const Home = () => {
+
+  const dispatch = useDispatch<AppDispatch>()
+
+  const { isLoading } = useSelector(weatherDataSelector)
+
+  //Sirve para buscar datos apenas se abrá la aplicación por primera vez.
+  const [fetchOnce, setFetchOnce] = useState(true)
+
+  useEffect(() => {
+    if(!isLoading) {setFetchOnce(false)}
+    if(!fetchOnce) return undefined
+    dispatch(fetchWeatherDataRedux('Puebla', 'metric'));
+  }, [isLoading])
+  
   return (
     <div className='Home-container'>
-        <Sidebar />
-        <Dashboard />
+      {
+        isLoading && fetchOnce
+        ? <Loading /> 
+        : <>
+            <Sidebar />
+            <Dashboard />
+          </>
+      }
     </div>
   )
 }
