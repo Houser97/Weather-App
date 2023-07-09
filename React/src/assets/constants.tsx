1107c9489813e4fe34bd625df92467894e9fad83
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-09 14:32:18 -0600

    Creater constants file
    This file stores data that is used throughout the application to be
    mapped or accessed to retrieve icons, descriptions and variables.

---
+++ React/src/assets/constants.tsx
@@ -0,0 +1,45 @@
+export const METRIC_OPTIONS = [{option: 'metric', unit: '°C'}, {option: 'imperial', unit: '°F'}]
+export const FORECAST_OPTIONS = ['daily', 'hourly']
+
+export const forecastDailySet = [
+    {
+        set: 'set1',
+        number: 1
+    },
+    {
+        set: 'set2',
+        number: 2
+    },
+    {
+        set: 'set3',
+        number: 3
+    },
+    {
+        set: 'set4',
+        number: 4
+    },
+]
+
+export const units = {
+    pressure: 'hPa',
+    humidity: '%',
+    wind_metric: 'km/h',
+    wind_imperial: 'mph',
+    temp_metric: '°C',
+    temp_imperial: '°F',
+}
+
+export const weatherDataArray = [
+    {
+        icon: 'Humidity',
+        name: 'Humidity'
+    },
+    {
+        icon: 'WindSpeed',
+        name: 'Wind Speed'
+    },
+    {
+        icon: 'Barometer',
+        name: 'Pressure'
+    }
+]
