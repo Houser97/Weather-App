133e9994c67050050a14d2a486a020c06613cfeb
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-09 10:36:21 -0600

    Use isDark redux state to toggle dark class

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,9 +1,14 @@
+import { useSelector } from 'react-redux'
 import './App.css'
 import Home from './components/Home'
+import { themeSelector } from './redux/slices/theme'
 
 function App() {
+
+  const { isDark } = useSelector(themeSelector)
+
   return (
-    <div className='app'>
+    <div className={`app ${isDark && 'dark'}`}>
       <Home/>
     </div>
   )
