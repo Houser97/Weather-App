267e11ab37998c42e484d92f58af6b52275cc476
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-15 12:23:00 -0600

    Set Loading component when changing daily forecast

--- React/src/components/ForecastOptions.tsx
+++ React/src/components/ForecastOptions.tsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { capitalizeFirstLetter } from '../assets/apiWeatherFunctions'
 import { FORECAST_OPTIONS } from '../assets/constants'
 import { filterSelector, updateForecastFilter } from '../redux/slices/filter'
+import { setIsLoading } from '../redux/slices/weather'
 import '../styles/ForecastOptions.css'
 
 const ForecastOptions = () => {
@@ -10,6 +11,14 @@ const ForecastOptions = () => {
     
     const { forecastOption } = useSelector(filterSelector)
 
+    const handleClick = (option: string) => {
+        dispatch(setIsLoading(true))
+        dispatch(updateForecastFilter(option))
+        setTimeout(() => {
+            dispatch(setIsLoading(false))
+        }, 300);
+    }
+
   return (
     <div className='ForecastOptions-container'>
         {
@@ -17,7 +26,7 @@ const ForecastOptions = () => {
                 return(
                     <div 
                      className={`forecast-option ${option === forecastOption && 'selected'}`}
-                     onClick={() => dispatch(updateForecastFilter(option))}
+                     onClick={() => handleClick(option)}
                      key={`forecast-option-${index}`}
                     >
                         {capitalizeFirstLetter(option)}
