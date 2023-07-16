cf182b05d911f3f8ec6f55d29056bfb3ba146989
Arturo Rivera <arturo.riverar97@gmail.com>
2023-07-16 07:54:36 -0600

    Get UVI value to fill gauge accordignly with css

--- React/src/components/Gauge.tsx
+++ React/src/components/Gauge.tsx
@@ -1,13 +1,31 @@
+import { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux'
+import { weatherDataSelector } from '../redux/slices/weather'
 import '../styles/Gauge.css'
 
+const UVI_MAX = 14;
+const UVI_MIN = 0;
+
 const Gauge = () => {
+
+  const { current } = useSelector(weatherDataSelector)
+  const fillRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const UVI = current.uvi > UVI_MAX ? 1 : current.uvi/UVI_MAX
+    if(fillRef.current){
+      fillRef.current.style.transform = `rotate(${UVI/2}turn)`
+    }
+  }, [current.city])
+  
+
   return (
     <div className='gauge__container'>
       <h3 className='gauge__title'>UVI</h3>
       <div className="gauge__body">
         <div className="gauge">
-          <div className="gauge__fill"></div>
-          <div className="gauge__cover"></div>
+          <div ref={fillRef} className="gauge__fill"></div>
+          <div className="gauge__cover">{current.uvi}</div>
         </div>
       </div>
     </div>
